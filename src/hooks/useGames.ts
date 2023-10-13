c139bac7ed2bdb0e9744b30e8f4d95da65f86d5a
CuiPeng <2891167663@qq.com>
2023-10-13 20:49:08 +0800

    Implement seaching games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,6 @@
 import { GameQuery } from './../App';
 import useData from "./useData";
 
-
-
-
 export interface Platform {
     id: number;
     name: string;
@@ -18,14 +15,13 @@ export interface Game {
     metacritic: number
 }
   
-
-
 const useGames = (gameQuery: GameQuery) => 
 useData<Game>('/games',{ 
     params: { 
         genres: gameQuery.genre?.id, 
         platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText
     }
     }, [
        gameQuery
